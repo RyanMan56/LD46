598becdabb0fd486d4cc2677b56f09da839f1d13
Ryan Jones <RyanMan56@gmail.com>
2020-04-20 14:06:15 +0100

    Jerry, camera, house collisions

---
+++ Assets/Scripts/Jerry.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jerry : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float upwardsForce = 7.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector2.up * upwardsForce);
+    }
+}
